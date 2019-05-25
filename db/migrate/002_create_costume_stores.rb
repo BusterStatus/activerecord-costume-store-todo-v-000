a5836dcfcf3a828867e65d1a92c726e66de2571d
matthew.serre <matthew.serre@gmail.com>
2019-05-25 02:15:36 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,9 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :number_of_costumes
       t.integer :number_of_employees
-      t.boolean
-      t.timestamps
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
   
