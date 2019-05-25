f9cbd2a14980ae34232b9390444dcef500f4fd1b
matthew.serre <matthew.serre@gmail.com>
2019-05-25 02:23:33 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.integer "number_of_costumes"
-    t.integer "number_of_employees"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.text "long_description"
+    t.text "description"
   end
 
 end
