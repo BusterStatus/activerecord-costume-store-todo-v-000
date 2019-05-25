3e8606c18d728fc1b450638250220e96b38d2017
matthew.serre <matthew.serre@gmail.com>
2019-05-25 02:16:48 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,7 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.float :price
       t.boolean :family_friendly
       t.datetime :opening_date
-      t.datetime :closing_time
+      t.datetime :closing_date
       t.text :long_description
     end
   end
